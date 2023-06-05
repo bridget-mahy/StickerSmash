bae4bd13b16d5f806053cb4e9047008ec10f3d98
Bridget Mahy <biddy.mahy@gmail.com>
2023-06-05 12:09:06 +1200

    step 4 - display an image on Build a Screen

--- App.js
+++ App.js
@@ -19,6 +19,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 })
